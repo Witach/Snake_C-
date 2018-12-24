81f0f6de07522a2fabfade22c322e2b3053e8fc6
Witach <witaszek98@wp.pl>
2018-12-25 00:01:27 +0100

    handle event,timer added to CSnake class

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,18 +1,49 @@
 #include "snake.h"
 
 CSnake::CSnake(CRect r, char _c /*=' '*/):
-  CFramedWindow(r, _c)
+  CFramedWindow(r, _c),course(0,1)
 {
-	czlony.push_back(CPoint(1,1));
-	czlony.push_back(CPoint(1,2));
-	czlony.push_back(CPoint(1,3));
+	segments.push_back(CPoint(1,1));
+	segments.push_back(CPoint(1,2));
+	segments.push_back(CPoint(1,3));
 }
 void CSnake::paint()
 {
 	CFramedWindow::paint();	
-	for(int i=0;i<czlony.size();i++)
+	for(int i=0;i<segments.size();i++)
 	{
-		gotoyx(czlony[i].y+geom.topleft.y,czlony[i].x+geom.topleft.x);
+		gotoyx(segments[i].y+geom.topleft.y,segments[i].x+geom.topleft.x);
 		printc('+');
 	}
+	t()?snake_move():;
+}
+bool CSnake::handleEvent(int key)
+{
+	if(CFramedWindow::handleEvent(key))
+	{
+		return true;
+	}
+	else
+	{
+		switch (key)
+		{
+			case 'w':
+				course=CPoint (0, -1);
+				break;
+			case 's':
+				course=CPoint (0, 1);
+				break;
+			case 'd':
+				course=CPoint (1, 0);
+				break;
+			case 'a':
+				course=CPoint (-1, 0);
+				break;
+		};
+	}
+	return false;
+}
+void CSnake::move_snake()
+{
+	
 }
--- src/snake.h
+++ src/snake.h
@@ -5,12 +5,31 @@
 #include<vector>
 #include"cpoint.h"
 #include"screen.h"
+class Timer
+{
+	int clock;
+	int refresh;
+	Timer(_refresh=5):clock(0),refresh(_refresh){}
+	bool operator()()
+	{
+		if(++clock==refresh)
+		{
+			clock=0;
+			return true;
+		}
+		return false;
+	}
+}
 class CSnake:public CFramedWindow
 {
-	vector<CPoint> czlony;
+	vector<CPoint> segments;
+	CPoint course;
+	Timer i;
 	public:
 	CSnake(CRect r, char _c = ' ');
   	void paint();
+	bool handleEvent(int key);
+	void move_snake();
 };
 
 #endif
